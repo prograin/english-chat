69ee84829a9c5511c9d4f06c54adbc41aa4350db
unknown <ahmadreza.pcg1377@gmail.com>
2025-10-31 16:58:06 +0330

    enhanc(ui)

--- frontends/web/src/features/home/pages/Main.jsx
+++ frontends/web/src/features/home/pages/Main.jsx
@@ -14,6 +14,16 @@ export default function MainHomePage() {
       <h1 className="text-5xl sm:text-6xl font-bold mb-10 text-center">Welcome to Home Page</h1>
 
       <div className="flex flex-col sm:flex-row gap-6 w-full max-w-lg justify-center">
+        <button
+          className="w-full sm:w-36 py-3 text-lg font-medium
+                     bg-white/20 backdrop-blur-sm text-white
+                     rounded-md border-2 border-transparent
+                     hover:border-gradient-to-r hover:from-purple-400 hover:via-pink-400 hover:to-red-400
+                     transition-all duration-300 hover:scale-105"
+          onClick={() => navigate("auth/telegram/login")}
+        >
+          Login
+        </button>
         {/* Profile button */}
         <button
           className="w-full sm:w-36 py-3 text-lg font-medium
